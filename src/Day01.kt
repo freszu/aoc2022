d31c5f1517326a4a3c7655e2bf57f7d86b9ccd91
Rafal Naniewicz <rafal.naniewicz@gmail.com>
2022-12-01 20:41:25 +0100

    Solve day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.map { it.toIntOrNull() }
+        .fold(0 to 0) { (max, current), value ->
+            if (value != null) {
+                Pair(max, current + value)
+            } else {
+                Pair(if (current > max) current else max, 0)
+            }
+        }.first
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(inputFileName: String): Int = readInputAsString(inputFileName)
+        .split("\n\n")
+        .map { it.split("\n") }
+        .map { elf -> elf.map { it.toIntOrNull() ?: 0 }.sum() }
+        .sortedDescending()
+        .take(3)
+        .sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    println(part1(readInput("Day01")))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    check(part2("Day01_test") == 45000)
+    println(part2("Day01"))
 }
